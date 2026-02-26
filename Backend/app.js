3bd0b60862a2a99a6baa6563208cd698c0d3f3a7
Mansuri786-Jiyan <mansuriziyan8888@gmail.com>
2026-02-26 11:23:54 +0530

    Refactor code structure for improved readability and maintainability

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,47 @@
-const file = "File importing..."
-console.log(file);
+// const file = "File importing..."
+// console.log(file);
 
-const user = require("./users")
+// const user = require("./users")
 
-console.log(user.userName);
-console.log(user.age);
-// const num = user.userData(10)
-console.log(user.userData);
+// console.log(user.userName);
+// console.log(user.age);
+// // const num = user.userData(10)
+// console.log(user.userData);
 
+const express = require("express");
+
+const app = express();
+
+const users = [
+  { id: 1, name: "Mansuri" },
+  { id: 2, name: "Barsena" },
+  { id: 3, name: "Ansari" },
+];
+
+const Employees = [
+  { id: 1, name: "Mansuri",sallary:30000 },
+  { id: 2, name: "Barsena",sallary:40000 },
+  { id: 3, name: "Ansari",sallary:20000 },
+];
+
+app.get("/test", (req, res) => {
+  res.send("Server is Running");
+});
+
+app.get("/users", (req, res) => {
+  res.json({
+    message:"Successfull fetch",
+    data:users
+  })
+});
+
+app.get("/employees", (req, res) => {
+  res.json({
+    message:"Successfull fetch",
+    data:Employees
+  })
+});
+
+app.listen(10000, (req, res) => {
+  console.log(`Server start on 10000`);
+});
