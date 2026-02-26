53f17548040dfe677e688a353a16787d7a3e8ed5
Mansuri786-Jiyan <mansuriziyan8888@gmail.com>
2026-02-26 09:30:04 +0530

    Implement user module and logging in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,10 @@
-console.log("Backend");
+const file = "File importing..."
+console.log(file);
+
+const user = require("./users")
+
+console.log(user.userName);
+console.log(user.age);
+// const num = user.userData(10)
+console.log(user.userData);
+
---
+++ Backend/users.js
@@ -0,0 +1,10 @@
+const userName = "Mansuri";
+const age = 21;
+
+const userData = (x) => {
+    console.log("User data called...");
+    return x;
+}
+
+module.exports = {userName,age,userData}
+// require("./users")
